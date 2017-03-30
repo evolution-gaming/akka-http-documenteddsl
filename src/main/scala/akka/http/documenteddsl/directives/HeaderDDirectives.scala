91e4095adf86e75f48826d9254b99757bde7c2e1
Andrey Onistchuk <aonistchuk@evolutiongaming.com>
2017-03-31 02:04:27 +0300

    add header by type

--- src/main/scala/akka/http/documenteddsl/directives/HeaderDDirectives.scala
+++ src/main/scala/akka/http/documenteddsl/directives/HeaderDDirectives.scala
@@ -5,14 +5,16 @@ import akka.http.scaladsl.model.HttpHeader
 import akka.http.scaladsl.model.headers.ModeledCompanion
 import akka.http.scaladsl.server._
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.directives.HeaderMagnet
 import akka.http.scaladsl.unmarshalling._
 import org.coursera.autoschema.AutoSchema
 
 import scala.reflect.runtime.{universe => ru}
 
 trait HeaderDDirectives {
+  sealed trait Header[T] extends DDirective1[T]
 
-  case class Header(name: String, acceptedValues: String*) extends DDirective1[String] {
+  case class HeaderByName(name: String, acceptedValues: String*) extends Header[String] {
     def describe(w: RouteDocumentation)(implicit as: AutoSchema): RouteDocumentation = {
       w.header(name, required = true, constraints = if (acceptedValues.isEmpty) None else Some(acceptedValues.toSet))
     }
@@ -21,9 +23,19 @@ trait HeaderDDirectives {
     }
   }
 
+  case class HeaderByType[T](m: HeaderMagnet[T], acceptedValues: T*) extends Header[T] {
+    def describe(w: RouteDocumentation)(implicit as: AutoSchema): RouteDocumentation = {
+      w.header(m.headerName, required = true, constraints = if (acceptedValues.isEmpty) None else Some(acceptedValues.toSet map {_.toString}))
+    }
+    def delegate: Directive1[T] = {
+      if (acceptedValues.isEmpty) headerValueByType(m) else headerValueByType(m) filter acceptedValues.contains
+    }
+  }
+
   object Header {
-    def apply(s: Symbol, acceptedValues: String*): Header = Header(s.name, acceptedValues:_*)
-    def apply[T](s: ModeledCompanion[T], acceptedValues: String*): Header = Header(s.name, acceptedValues:_*)
+    def apply(name: String, acceptedValues: String*): Header[String]  = HeaderByName(name, acceptedValues:_*)
+    def apply(s: Symbol, acceptedValues: String*): Header[String]     = HeaderByName(s.name, acceptedValues:_*)
+    def apply[T](m: HeaderMagnet[T], acceptedValues: T*): Header[T]   = HeaderByType(m, acceptedValues:_*)
   }
 
   case class OptHeader(name: String, acceptedValues: String*) extends DDirective1[Option[String]] {
