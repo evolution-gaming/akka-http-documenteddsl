319198ced3d43700565023279243d83b727a9e3c
Scala Steward <me@scala-steward.org>
2021-10-15 14:28:58 +0200

    Update akka-stream, akka-stream-testkit to 2.6.17

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val AkkaHttpVersion = "10.1.11"
-  val AkkaVersion = "2.6.3"
+  val AkkaVersion = "2.6.17"
   val akkaHttpCore      = "com.typesafe.akka"   %% "akka-http-core"       % AkkaHttpVersion
   val akkaHttp          = "com.typesafe.akka"   %% "akka-http"            % AkkaHttpVersion
   val akkaHttpTestKit   = "com.typesafe.akka"   %% "akka-http-testkit"    % AkkaHttpVersion % Test
