5e18d76ba0dae1fbf7a9f0692ba36c65610f4cff
Mareks Rampāns <8796159+mr-git@users.noreply.github.com>
2024-07-18 14:04:23 +0300

    add release action

--- build.sbt
+++ build.sbt
@@ -5,15 +5,21 @@ import BuildSettings._
 import Dependencies._
 import Examples._
 
+val alias: Seq[sbt.Def.Setting[_]] =
+  //  addCommandAlias("check", "all versionPolicyCheck Compile/doc") ++
+  addCommandAlias("check", "show version") ++
+    addCommandAlias("build", "+all compile test")
+
 lazy val project = (Project(artifactId, file("."))
   configs ExamplesConfig
   settings inConfig(ExamplesConfig)(compileBase ++ compileSettings ++ Seq(
   run := Defaults.runTask(fullClasspath in ExamplesConfig, mainClass in run, runner in run).evaluated,
   runMain := Defaults.runMainTask(fullClasspath in ExamplesConfig, runner in run).evaluated))
+  settings alias
   settings basicSettings
   settings Seq(
-    resolvers += Resolver.bintrayRepo("evolutiongaming", "maven"),
-    resolvers += Resolver.bintrayRepo("hseeberger", "maven"),
+//    resolvers += Resolver.bintrayRepo("evolutiongaming", "maven"),
+//    resolvers += Resolver.bintrayRepo("hseeberger", "maven"),
     libraryDependencies ++= Seq(
       akkaHttpCore, akkaHttp, akkaHttpTestKit, akkaHttpPlayJson,
       akkaStream, akkaStreamTestkit,
--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -1,9 +1,8 @@
-import java.net.URL
-
 import sbt._
 import sbt.Keys._
-import bintray.BintrayPlugin.autoImport.bintrayOrganization
+import sbt.Defaults._
 import sbtrelease.ReleasePlugin.autoImport.releaseCrossBuild
+import com.evolution.artifactory.ArtifactoryPlugin.autoImport.ResolverOpsArtifactory
 
 object BuildSettings {
   val artifactId = "akka-http-documenteddsl"
@@ -11,13 +10,13 @@ object BuildSettings {
   lazy val basicSettings = Seq(
     name                  := artifactId,
     organization          := "com.evolutiongaming",
-    homepage              := Some(new URL("http://github.com/evolution-gaming/akka-http-documenteddsl")),
+    homepage              := Some(url("https://github.com/evolution-gaming/akka-http-documenteddsl")),
     startYear             := Some(2016),
-    organizationName      := "Evolution Gaming",
-    organizationHomepage  := Some(url("http://evolutiongaming.com")),
-    bintrayOrganization   := Some("evolutiongaming"),
+    organizationName      := "Evolution",
+    organizationHomepage  := Some(url("https://evolution.com")),
     scalaVersion          := crossScalaVersions.value.head,
     crossScalaVersions    := Seq("2.13.3", "2.12.12"),
     releaseCrossBuild     := true,
+    publishTo             := Some(Resolver.evolutionReleases),
     licenses              := Seq(("Apache-2.0", url("http://www.apache.org/licenses/LICENSE-2.0"))))
 }
