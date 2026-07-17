e4ab6664362b9832eb6756795fd93619dc68d224
Mareks Rampāns <8796159+mr-git@users.noreply.github.com>
2026-07-17 16:13:15 +0300

    drop Scala 2.12 support

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -15,7 +15,7 @@ object BuildSettings {
     organizationName      := "Evolution",
     organizationHomepage  := Some(url("https://evolution.com")),
     scalaVersion          := crossScalaVersions.value.head,
-    crossScalaVersions    := Seq("2.13.18", "2.12.12"),
+    crossScalaVersions    := Seq("2.13.18"),
     releaseCrossBuild     := true,
     publishTo             := Some(Resolver.evolutionReleases),
     licenses              := Seq(("Apache-2.0", url("http://www.apache.org/licenses/LICENSE-2.0"))))
