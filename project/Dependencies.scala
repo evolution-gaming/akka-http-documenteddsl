93b03757da4987e1dc7295faaafc3a3577efbb4a
Mareks Rampāns <8796159+mr-git@users.noreply.github.com>
2026-07-17 16:10:29 +0300

    update akka-http-play-json to 0.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val akkaHttpTestKit   = "com.typesafe.akka"   %% "akka-http-testkit"    % AkkaHttpVersion % Test
   val akkaStream        = "com.typesafe.akka"   %% "akka-stream"          % AkkaVersion
   val akkaStreamTestkit = "com.typesafe.akka"   %% "akka-stream-testkit"  % AkkaVersion % Test
-  val akkaHttpPlayJson  = "com.evolutiongaming" %% "akka-http-play-json"  % "0.1.13"
+  val akkaHttpPlayJson  = "com.evolutiongaming" %% "akka-http-play-json"  % "0.3.0"
   val jsonSchema        = "com.evolutiongaming" %% "autoschema"           % "1.0.5"
   val scalaTest         = "org.scalatest"       %% "scalatest"            % "3.0.8" % Test
   val mockito           = "org.mockito"          % "mockito-core"         % "3.4.4" % Test
