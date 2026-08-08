07ec554b20864f890b7524d96ffd80098111c176
sshevchenko <sshevchenko@evolution.com>
2026-08-08 14:04:35 +0200

    Bump autoschema to 2.0.0 (play-json 3)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   val akkaStream        = "com.typesafe.akka"   %% "akka-stream"          % AkkaVersion
   val akkaStreamTestkit = "com.typesafe.akka"   %% "akka-stream-testkit"  % AkkaVersion % Test
   val akkaHttpPlayJson  = "com.evolutiongaming" %% "akka-http-play-json"  % "0.3.0"
-  val jsonSchema        = "com.evolutiongaming" %% "autoschema"           % "1.0.5"
+  val jsonSchema        = "com.evolutiongaming" %% "autoschema"           % "2.0.0"
   val scalaTest         = "org.scalatest"       %% "scalatest"            % "3.2.20" % Test
   val mockito           = "org.mockito"          % "mockito-core"         % "5.23.0" % Test
 }
